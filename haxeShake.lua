f32d2148203cd1a4a4dc0d4278113f62ff370115
Adrigamer278 <72508080+Adrigamer278@users.noreply.github.com>
2022-12-25 18:17:48 +0100

    Update haxeShake.lua
    now using the updated version of the code that nof uses
    
    this version allows

--- haxeShake.lua
+++ haxeShake.lua
@@ -1,27 +1,45 @@
--- this kinda has support for when people do shake("Camera",)
+-- the license is the license is the license is the license is the license is the license is the license is the license is the license is the license 
+-- some kind of port of flxObj.shake() ; for a camera you can just put
 
 local currentObjShakeData = {}
-
 local function shake(obj,duration,intensity)
-	currentObjShakeData[obj] = {
-		startDuration = duration,
-		Duration = duration or 0,
-		Intensity = intensity,
-		Completed = false,
-		axesShake = {X=true,Y=true}
-	}
+	if currentObjShakeData[obj] then
+		currentObjShakeData[obj] = {
+			startDuration = duration,
+			Duration = duration or 0,
+			i=currentObjShakeData[obj].i+1 or 0;
+			Intensity = intensity,
+			startPos=currentObjShakeData[obj].startPos or obj.Position,
+			Completed = false,
+			axesShake = {X=true,Y=true}
+		}
+	else
+		currentObjShakeData[obj] = {
+			startDuration = duration,
+			Duration = duration or 0,
+			i=0;
+			startPos=obj.Position,
+			Intensity = intensity,
+			Completed = false,
+			axesShake = {X=true,Y=true}
+		}
+	end
+	
+	local FRAME_RATE=60 -- THE FRAME RATE (used to not cause the really happy incident for high fps)
+	
 	local shake = 0 -- we dont want to kill 240fps players
-	local originalPos = obj.Position
-	while not currentObjShakeData[obj].Completed do
+	local originalPos = currentObjShakeData[obj].startPos
+	local ind=currentObjShakeData[obj].i -- cancelation token (i think thats how its clalled)
+	while not currentObjShakeData[obj].Completed and currentObjShakeData[obj].i == ind do
 		if currentObjShakeData[obj].Duration>0 then
 			local dt = game["Run Service"].Heartbeat:Wait()
 			shake+=dt
 			currentObjShakeData[obj].Duration-=dt
-			if currentObjShakeData[obj].Duration<=0 then
+			if currentObjShakeData[obj].Duration<=0 or gameended then -- if duration <=0 or game stopped, then complete
 				currentObjShakeData[obj].Completed = true
 			else
-				if shake > (1/60) then
-					shake-=(1/60)
+				if shake > (1/FRAME_RATE) then
+					shake-=(1/FRAME_RATE)
 					-- proceed to shake
 					local _fxShakeIntensity=currentObjShakeData[obj].Intensity
 					local width,height = 
@@ -38,20 +56,24 @@ local function shake(obj,duration,intensity)
 						obj.Position = originalPos + newUdim
 					else
 						-- tweak this bc it can be real different
-						camera.shake = Vector3.new((newUdim.X.Offset/width)*3,(newUdim.Y.Offset/height)*3)
+						camera.shake = Vector3.new((newUdim.X.Offset/width)*30,(newUdim.Y.Offset/height)*30)
 					end
 				end
 			end
-			
+
 		else
 			if not currentObjShakeData[obj].Completed then
 				currentObjShakeData[obj].Completed = true
 			end
 		end
 	end
-	if obj ~= "Camera" then
-		obj.Position = originalPos
-	else
-		camera.shake = Vector3.new()
+	if currentObjShakeData[obj].i == ind then
+		-- reset!
+		if obj ~= "Camera" then
+			obj.Position = originalPos
+		else
+			camera.shake = Vector3.new()
+		end
+		currentObjShakeData[obj]=nil;
 	end
 end
